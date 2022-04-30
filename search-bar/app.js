cf4a82380e31d98045e3ccd7aa5cefc657c5e0e0
Johnsonj91 <Jordan33111@gmail.com>
2022-04-30 18:08:46 -0400

    Live search for user names added

--- search-bar/app.js
+++ search-bar/app.js
@@ -1,10 +1,22 @@
 const input = document.getElementById('searchInput');
-let searchQuery
+let searchQuery;
 let allResults = document.querySelectorAll(".name");
-console.log(allResults[0].textContent);
-input.addEventListener('keyup', function(e) {
-    searchQuery = e.target.value.toLowerCase();
-    console.log(searchQuery);
-})
+//console.log(allResults[0].textContent);
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+        
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
+    }
+});
+
+
 
 

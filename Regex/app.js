edcc07ce53b994a03f5fa05dbadc45834490857c
Johnsonj91 <Jordan33111@gmail.com>
2022-04-30 13:56:44 -0400

    buidlind a Modal first commit

---
+++ Regex/app.js
@@ -0,0 +1,30 @@
+let sentence = "The dog chased the cat."
+let regex = /the/
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); 
+
+console.log(result);
+
+
+
+
+//Or in Regex
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let result2 = petRegex.test(petString);
+
+
+let myString2 = "freeCodeCamp";
+
+let fccRegex = /freecodecamp/i; // Change this line
+
+let result3 = fccRegex.test(myString2);
+
+
+
+
+
+
---
+++ building-model/README.md
@@ -0,0 +1,12 @@
+
+
+
+Building a Modal
+
+Created the modal and overlay in the html
+adding styling to buttons 
+
+
+
+
+
